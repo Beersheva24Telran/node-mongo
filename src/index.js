c14167badb06b70c8db49c4059cc3f00a009ac91
Yuragranovsky <yuragranovsky@gmail.com>
2025-02-24 16:29:47 +0200

    complex queries for MongoDB

--- src/index.js
+++ src/index.js
@@ -3,66 +3,93 @@ const {
   MONGO_CONNECTION,
   MONGO_PASSWORD,
   MONGO_CLUSTER,
-  DB_NAME,
-  COURSES_COLLECTION,
-  ACCOUNTS_COLLECTION,
+  DB_NAME
 } = process.env;
 const connectionString = `${MONGO_CONNECTION}:${MONGO_PASSWORD}@${MONGO_CLUSTER}`;
 const mongoConnection = new MongoConnection(connectionString, DB_NAME);
-const accounts = mongoConnection.getCollection(ACCOUNTS_COLLECTION);
-const courses = mongoConnection.getCollection(COURSES_COLLECTION);
-const courseObjects = {
-  J101: { id: "J101", name: "Front-End", lecturer: "Vasya", hours: 200 },
-  J102: { id: "J102", name: "JAVA", lecturer: "Vasya", hours: 300 },
-  J103: { id: "J103", name: "Back-End", lecturer: "Olya", hours: 350 },
-  J104: { id: "J104", name: "Node", lecturer: "Olya", hours: 150 },
-  J105: { id: "J105", name: "AWS", lecturer: "Vova", hours: 200 },
-  J106: { id: "J106", name: "C++", lecturer: "Vova", hours: 500 },
-};
-async function insertCourses() {
-  try {
-    for  (const course of Object.values(courseObjects)) {
-      await courses.insertOne(toDbCourse(course));
-
+const collectionMovies = mongoConnection.getCollection("movies");
+const collectionComment = mongoConnection.getCollection("comments");
+async function getMovies2010ComedyRatingGreaterAvg() {
+  return await collectionMovies.aggregate([
+    { //stage $matchfor filtering only movies tha have imdb.rating different from empty string
+      '$match': {
+        'imdb.rating': {
+          '$ne': ''
+        }
+      }
+    }, {//stage $group for getting average rating for all movies and all embeded documents containing title, rating, year and genres
+      '$group': {
+        '_id': null, 
+        'avgRating': {
+          '$avg': '$imdb.rating'
+        }, 
+        'documents': {
+          '$push': {
+            'title': '$title', 
+            'rating': '$imdb.rating', 
+            'year': '$year', 
+            'genres': '$genres'
+          }
+        }
+      }
+    }, { //stage for unwinding array of documents
+      '$unwind': {
+        'path': '$documents'
+      }
+    }, { //matching stage for filtering all movies released at 2010, having genre Comedy and imdb.rating greater than the average value
+      '$match': {
+        '$and': [
+          {
+            'documents.year': 2010, 
+            'documents.genres': 'Comedy'
+          }, {
+            '$expr': {
+              '$gt': [
+                '$documents.rating', '$avgRating'
+              ]
+            }
+          }
+        ]
+      }
+    }, {
+      '$project': { //required projection of only title and rating
+        'title': '$documents.title', 
+        'rating': '$documents.rating', 
+        '_id': 0
+      }
     }
-    console.log("courses inserted");
-  } catch (error) {
-     console.log(error);
-  } finally {
-     mongoConnection.close();
-  }
+  ]).sort({"rating":-1}).toArray()
 }
-
-function toDbCourse(course) {
-  const { name, lecturer, hours, id } = course;
-  return { _id: id, name, lecturer, hours };
-}
-// insertCourses();
-async function findCourse(courseId) {
-    const course = await courses.findOne({_id:courseId})
-    if(!course) {
-        throw Error(`course ${courseId} not found`);
+async function getCommentsWithMovieTitleInsteadMovieId(){
+  return collectionComment.aggregate([
+ 
+  {
+      '$lookup': {
+          'from': 'movies',
+          'localField': 'movie_id',
+          'foreignField': '_id',
+          'as': 'movies'
+      },
+    },
+      {
+        '$match': {
+            'movies.0': {
+                '$exists':true
+            }
+        }
+    }, {
+      '$limit': 5
+  }, {
+       '$addFields':{'movie_title':{$arrayElemAt: ["$movies.title",0]}}
+    }, {
+        '$project': {
+            'movies': 0,
+            'movie_id': 0
+        }
     }
-    return course;
-}
-//findCourse("J11").then((course) => console.log(course)).catch(e => console.log(e.message)).finally(()=>mongoConnection.close());
-async function findFilteredCourses(filter) {
-    const coursesRes = await courses.find(filter).toArray();
-    return coursesRes
-}
-//findFilteredCourses({lecturer: "Vasya", hours:200}).then((course) => console.log(course)).catch(e => console.log(e.message)).finally(()=>mongoConnection.close()) 
-async function updateCourse(courseId, updaterObj) {
-    const course = await courses.findOneAndUpdate({_id:courseId}, {$set:updaterObj}, {returnDocument: "after"});
-    return course;
-}
-// const course = await updateCourse("J101",{hours:400});
-async function deleteCourse(courseId) { 
-    const res = await courses.findOneAndDelete({_id:courseId});
-    if(!res) {
-        throw Error(`course ${courseId} not found`)
-    }
-    return res;
-    
-}
-
-deleteCourse("J101").then(res => console.log(res)).catch(e => console.log(e.message)).finally(()=>mongoConnection.close())
+  
+  
+]).toArray();}
+//console.log(await getMovies2010ComedyRatingGreaterAvg());
+console.log(await getCommentsWithMovieTitleInsteadMovieId())
+mongoConnection.close();
